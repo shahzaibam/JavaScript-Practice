cf81585e3cb669ae39ae175dbe5ee0714d88b5e0
Shah <shebiraaja@gmail.com>
2022-07-06 21:22:43 +0200

    intenatndo obtener numeros de un usuario y ordenarlos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,36 @@
-let text = "AAABBBCCCDDDEEEFFFGGGHHHIIIJJJKKKLLLMMMNNNOOO";
-console.log(text.length);
+// let text = "AAABBBCCCDDDEEEFFFGGGHHHIIIJJJKKKLLLMMMNNNOOO";
+// console.log(text.length);
 
-let stri = "Bananas, Kiwis";
-let particio = stri.slice(7, 14);
-console.log(particio);
+// let stri = "Bananas, Kiwis";
+// let particio = stri.slice(7, 14);
+// console.log(particio);
 
-let palabra = "Shzaib";
-console.log(palabra.replace("Shzaib", "Shah Zaib"));
+// let palabra = "Shzaib";
+// console.log(palabra.replace("Shzaib", "Shah Zaib"));
 
-let palabra_dos = "quiero ser mayuscula";
-console.log(palabra_dos.toUpperCase());
+// let palabra_dos = "quiero ser mayuscula";
+// console.log(palabra_dos.toUpperCase());
 
-let palabra_tres = "QUIERO SER MINUSCULA";
-console.log(palabra_tres.toLowerCase());
+// let palabra_tres = "QUIERO SER MINUSCULA";
+// console.log(palabra_tres.toLowerCase());
 
-let palabra_cuatro = "Concatename con la palabra cinco";
+// let palabra_cuatro = "Concatename con la palabra cinco";
 
-let palabra_cinco = ", No quiero";
+// let palabra_cinco = ", No quiero";
 
-console.log(palabra_cuatro.concat(palabra_cinco));
+// console.log(palabra_cuatro.concat(palabra_cinco));
 
-let palabra_seis = "Hola Me llamo Shah y estoy programando";
-console.log(palabra_seis.indexOf("programando")); //empieza buscando desde el principio
+// let palabra_seis = "Hola Me llamo Shah y estoy programando";
+// console.log(palabra_seis.indexOf("programando")); //empieza buscando desde el principio
 
-let palabra_siete = "Hola me llamo Shah y estoy programando";
-console.log(palabra_siete.lastIndexOf("programando", 26)); //le estoy diciendo que empiece a contar desde la opcion 26 hacia atrás, como no lo encuentra sale -1
+// let palabra_siete = "Hola me llamo Shah y estoy programando";
+// console.log(palabra_siete.lastIndexOf("programando", 26)); //le estoy diciendo que empiece a contar desde la opcion 26 hacia atrás, como no lo encuentra sale -1
 
-let nombre = "Shah Zaib";
-let primer_apellido = "Asghar";
-let segundo_apellido = "Munir";
+// let nombre = "Shah Zaib";
+// let primer_apellido = "Asghar";
+// let segundo_apellido = "Munir";
 
-console.log(`Hola Sr. ${nombre}, es verdad que sus apellidos son, ${primer_apellido} y ${segundo_apellido}`);
+// console.log(`Hola Sr. ${nombre}, es verdad que sus apellidos son, ${primer_apellido} y ${segundo_apellido}`);
 
 
 //pedir numeros y sumarlos
@@ -97,8 +97,8 @@ console.log(`Hola Sr. ${nombre}, es verdad que sus apellidos son, ${primer_apell
 // console.log(lent.length)
 
 
-const cars = []
-console.log(cars.length)
+// const cars = []
+// console.log(cars.length)
 
 // cars.push("Saab", "Volvo", "Seat", "Mercedes", "Bugatti", "Range Rover", "Honda", "Ferrari", "Ford", "Peugeot")
 // console.log(cars.length)
@@ -113,8 +113,7 @@ console.log(cars.length)
 // }
 
 // for (let i=1; i<11; i++) {
-//     cars.push(prompt(`${i} Escribe diez coches`))
-    
+//     cars.push(prompt(`${i} Escribe diez coches`)) 
 //     document.write(`
 //         <p>
 //             ${i}. ${cars[i-1]}
@@ -145,7 +144,7 @@ console.log(cars.length)
 // console.log(bubbleSort(num))
 
 //          [0,1,2,3,4,5,6]
-const num = [2,1,10,5,4,3,0]
+// const num = [2,1,10,5,4,3,0]
 //           1,2,5,4,3,0,10 --> primera vuelta,, índice 0 del for 1
 //           1,2,4,3,0,5,10 --> segunda vuelta,, índice 1 del for 1
 //           1,2,3,0,4,5,10 --> tercera vuelta,, índice 2 del for 1
@@ -153,7 +152,20 @@ const num = [2,1,10,5,4,3,0]
 //           1,0,2,3,4,5,10 --> quinta vuelta,, índice 4 del for 1
 //           0,1,2,3,4,5,10 --> sexta vuelta,, índice 5 del for 1
 
+const num = []
+
+for (let i=1; i<15; i++) {
+    num.push(prompt(`${i}. escribe un numero`)) 
+}
+
+console.log(num)
+document.write(`
+    ${num} <br> <br> 
+    ${num.length} <br> <br>
+    `);
+
 function bubbleSort (n) {
+
     for (let i = 0; i < n.length - 1; i++) {
         for (let j = 0; j < n.length - 1; j++) {
             if (n[j] > n[j+1]) {
@@ -165,7 +177,11 @@ function bubbleSort (n) {
     }
     return n;
 }
-console.log(bubbleSort(num));
+console.log(bubbleSort(num))
+
+document.write(`<br/> <br/>
+    ${bubbleSort(num)}
+    `);
 
 
 
